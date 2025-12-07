357a5170d4e2e51c59eb01825fcfbea15e1d52ec
Hexa <ghstdu@gmail.com>
2025-12-07 12:42:01 +0100

    new icons

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -1,5 +1,5 @@
 export {default as homeIcon} from "./homeIcon"
-export {default as inventoryIcon} from "./inventoryIcon"
-export {default as shopIcon} from "./shopIcon"
+export {default as usersIcon} from "./usersIcon"
+export {default as reportIcon} from "./reportIcon"
 export {default as userIcon} from "./userIcon"
-export {default as supplierIcon} from "./supplierIcon"
+export {default as meetingIcon} from "./meetingIcon"
