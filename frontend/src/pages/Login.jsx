b74497f3adc33f35b3dfc88fdab9eb726eda09a3
Hexa <ghstdu@gmail.com>
2025-12-14 13:30:47 +0100

    added login and loading check on main page with permission for each navbar button

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import api from '../api/axios'
+import {useNavigate} from 'react-router-dom'
 
 const Login = () => {
+
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState(false);
+  const navigate = useNavigate()
+
+  const login = async (email, password) =>{
+   await api.get('/sanctum/csrf-cookie')
+
+    const response = await api.post('/login', {
+      "email":email,
+      "password":password
+    })
+
+    if (response.status == 200){
+      navigate("/main")
+    } else if ( response.status == 401){
+
+    }
+  }
+
+
   return (
     <div className='flex flex-row items-center min-h-screen overflow-hidden bg-white'>
       <div className="flex flex-col items-center justify-center w-1/2 min-h-screen bg-linear-to-bl from-primary via-[#1976D2] to-secondary shadow-[0_4px_50px_25px_rgba(0,0,0,0.1)] relative overflow-hidden">
@@ -12,16 +35,15 @@ const Login = () => {
       <div className="flex flex-col items-center rounded-[5px] min-w-[400px] py-[30px] w-1/2 space-y-3 p-15">
           <h1 className="text-xl font-bold mb-10 w-90">Connectez vous a votre compte !</h1>
           <div className="relative">
-            <input className="px-3 border border-gray-300 outline-0 rounded-md p-[3px] focus:border-primary w-90 h-11" type="email" placeholder='Entrez ici'></input>
+            <input className={`px-3 border border-gray-300 outline-0 rounded-md p-[3px] focus:border-primary w-90 h-11`} type="email" placeholder='Entrez ici' onChange={(e)=>{ setEmail(e.target.value) }}></input>
             <label className="absolute -top-2 left-3 text-gray-400 text-[10px] bg-white px-1">Email</label>
           </div>
           <div className="relative">
-            <input className="px-3 border border-gray-300 outline-0 rounded-md focus:border-primary w-90 h-11" type="password" placeholder='Entrez ici'></input>
+            <input className={`px-3 border border-gray-300 outline-0 rounded-md focus:border-primary w-90 h-11`} type="password" placeholder='Entrez ici' onChange={(e)=>{ setPassword(e.target.value) }}></input>
             <label className="absolute -top-2 left-3 text-gray-400 text-[10px] bg-white px-1">Mots de passe</label>
           </div>
-          <button className="p-[7px] bg-primary rounded-md text-white font-bold text-[1.1em] text-md shadow-md hover:bg-[#037fc7] transition-all duration-200 w-90 h-12">Se connecter</button>
+          <button className="p-[7px] bg-primary rounded-md text-white font-bold text-[1.1em] text-md shadow-md hover:bg-[#037fc7] transition-all duration-200 w-90 h-12" onClick={()=>{login(email, password)}}>Se connecter</button>
           <span className="block w-70 h-px bg-gray-300 my-6"></span>
-
       </div>
     </div>
   )
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,40 +1,83 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/navbar'
 import Dashboard from '../compact-pages/dashboard'
 import Inventory from '../compact-pages/inventory'
 import {homeIcon, usersIcon, reportIcon, meetingIcon} from '../assets/icons/index'
+import api from "../api/axios"
+import { useNavigate } from 'react-router-dom'
 
 const Main = () => {
 
+  const navigate = useNavigate()
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const checkLogin = async ()=>{
+    try{
+      const response = await api.get("/user/check");
+      setUser(response.data)
+    } catch(err){
+      setUser(null)
+      navigate("/login")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  // logs in
+  useEffect(()=>{
+    checkLogin()
+  },[])
+
+  //check if loaded
+  useEffect(()=>{
+    if (user) {
+      setLoading(false)
+    }
+  },[user])
+
   const [currentPage, setcurrentPage] = useState(0)
-    const pages = [
-      {name:"Tableau de bord", icon:homeIcon},
-      {name:"Etudiants", icon:usersIcon},
-      {name:"Rapports", icon:reportIcon},
-      {name:"Soutenances", icon:meetingIcon},
-    ]
-    const compactPages = [
-      Dashboard,
-      Inventory
-    ]
 
-    const Placeholder = () => (
-      <div className="p-6 text-center text-accent opacity-50">Page not implemented yet</div>
+  const pages = [
+    {name:"Tableau de bord", icon:homeIcon, roles:[1,2,3]},
+    {name:"Etudiants", icon:usersIcon, roles:[2,3]},
+    {name:"Rapports", icon:reportIcon, roles:[1,2,3]},
+    {name:"Soutenances", icon:meetingIcon, roles:[2,3]},
+  ]
+  
+  const allowedPages = user ? pages.filter(page=>page.roles.includes(user.role)) : [];
+  console.log(user)
+
+  const compactPages = [
+    Dashboard,
+    Inventory
+  ]
+
+  const Placeholder = () => (
+    <div className="p-6 text-center text-accent opacity-50">Page not implemented yet</div>
+  )
+
+    if (loading){
+    return (
+      <div className="">
+        Loading ...
+      </div>
     )
+  }
 
-    const PageComponent = compactPages[currentPage] ?? Placeholder
+  const PageComponent = compactPages[currentPage] ?? Placeholder
 
 
   return (
     <div className="flex flex-row">
-      <Navbar pageList={pages} page={currentPage} changePage={setcurrentPage} />
+      <Navbar pageList={allowedPages} page={currentPage} changePage={setcurrentPage} />
       <div className="flex flex-col min-h-screen w-full">
           <div className="h-[10%] border-b border-accent">
             
           </div> 
           <div className="h-[90%]">
             <div className="bg-gray-100 w-full h-full overflow-hidden">
-              <PageComponent/>
+              <PageComponent userData={user}/>
             </div>
           </div>
       </div>
