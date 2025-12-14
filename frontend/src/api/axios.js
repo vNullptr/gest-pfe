66e2b3e6e377dd9b5e92ac39c19a30253ea66595
Hexa <ghstdu@gmail.com>
2025-12-14 14:25:47 +0100

    fixed axios interceptor and added wrong credentials handling

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -16,9 +16,6 @@ api.interceptors.response.use(
         return response
     },
     (error) => {
-        if (error.response?.status === 401) {
-            window.location.href = '/login';
-        }
         return Promise.reject(error);
     }
 );
