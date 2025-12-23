70ed4556bf16d8332ec09aeed843094e81e0ef89
vNullptr <ghstdu@gmail.com>
2025-12-23 23:17:31 +0100

    fix: added missing status to intership model

--- backend/database/migrations/2025_12_10_124507_create_stages_table.php
+++ backend/database/migrations/2025_12_10_124507_create_stages_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('stages', function (Blueprint $table) {
             $table->id();
             $table->string("entreprise");
+            $table->integer("statut");
             $table->date("debut");
             $table->date("fin");
             $table->timestamps();
