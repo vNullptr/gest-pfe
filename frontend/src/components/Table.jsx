574d83131b7a0795692e417bf094700bda8a0542
vNullptr <ghstdu@gmail.com>
2025-12-24 12:20:37 +0100

    add : finishing table component

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,31 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Table = ({tableHead, rowData}) => {
+const Table = ({Headers, Data}) => {
 
-  const listElements = []
-  for (let i = 0; i < 10; i++) {
-    listElements.push(
-    <tr className="[&>td]:p-2 [&>td]:text-left [&>td]:font-light [&>td]:text-sm [&>td]:border-t [&>td]:border-gray-200">
-      <td>Data</td>
-      <td>Data</td>
-      <td>Data</td>
-      <td>Data</td>
-    </tr>)
-  }
+  const [hovered, setHovered] = useState(null)
 
   return (
     <>
       <table className='bg-white w-full outline outline-gray-200 rounded-lg select-none'>
         <thead className="bg-tertiary">
-          <tr className=" [&>td]:p-2 [&>td]:font-bold [&>td]:text-left [&>td]:text-sm">
-            <td className="rounded-tl-lg">Head</td>
-            <td>Head</td>
-            <td>Head</td>
-            <td className="rounded-tr-lg">Head</td>
+          <tr className=" [&>th]:p-2 [&>th]:font-bold [&>th]:text-left [&>th]:text-sm">
+            {Headers.map(h=>(
+              <th key={h?.key} className={`w-[${h?.width}px]`}>{h?.label}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {listElements}
+          {
+            Data.map((d,i)=>(
+              <tr 
+              className="[&>td]:p-2 [&>td]:text-left [&>td]:font-light [&>td]:text-sm [&>td]:border-t [&>td]:border-gray-200"
+              onMouseEnter={()=>setHovered(i)}
+              onMouseLeave={()=>setHovered(null)}
+              >
+                {Headers.map(h=>(
+                  h?.action ? 
+                  <td key={h?.key} className="flex flex-row">
+                    {h?.action}
+                  </td>
+                  :
+                  (h?.tag ? 
+                  <td key={h?.key}>
+                    <div className="border border-gray-200 rounded-xl w-fit px-1">{d[h?.key]}</div>
+                  </td>
+                  :
+                  <td key={h?.key}>{d[h?.key]}</td>)
+                ))}
+              </tr>
+            ))
+          }
         </tbody>
       </table>
     </>
