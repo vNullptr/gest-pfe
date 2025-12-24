f8b6b09b3518ced50dcefc671e9d31d5f160977f
vNullptr <ghstdu@gmail.com>
2025-12-24 20:57:09 +0100

    refactor : improved the table model

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -10,30 +10,20 @@ const Table = ({Headers, Data}) => {
         <thead className="bg-tertiary">
           <tr className=" [&>th]:p-2 [&>th]:font-bold [&>th]:text-left [&>th]:text-sm">
             {Headers.map(h=>(
-              <th key={h?.key} className={`w-[${h?.width}px]`}>{h?.label}</th>
+              <th key={h?.key}>{h?.label}</th>
             ))}
           </tr>
         </thead>
         <tbody>
           {
-            Data.map((d,i)=>(
+            Data.map((row,i)=>(
               <tr 
               className="[&>td]:p-2 [&>td]:text-left [&>td]:font-light [&>td]:text-sm [&>td]:border-t [&>td]:border-gray-200"
               onMouseEnter={()=>setHovered(i)}
               onMouseLeave={()=>setHovered(null)}
               >
-                {Headers.map(h=>(
-                  h?.action ? 
-                  <td key={h?.key} className="flex flex-row">
-                    {h?.action}
-                  </td>
-                  :
-                  (h?.tag ? 
-                  <td key={h?.key}>
-                    <div className="border border-gray-200 rounded-xl w-fit px-1">{d[h?.key]}</div>
-                  </td>
-                  :
-                  <td key={h?.key}>{d[h?.key]}</td>)
+                {Headers.map(col=>(
+                  <td key={col?.key}>{col?.val(row)}</td>
                 ))}
               </tr>
             ))
