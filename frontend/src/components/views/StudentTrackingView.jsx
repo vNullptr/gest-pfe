88930dd589721832944e093f420cec1998a8b150
vNullptr <ghstdu@gmail.com>
2025-12-24 20:58:50 +0100

    add : new student progress tracking view

---
+++ frontend/src/components/views/StudentTrackingView.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import Table from '../Table'
+import api from '../../api/axios'
+
+const StudentTrackingView = () => {
+
+    const [students, setStudents] = useState([])
+    const [supervisors, setSupervisors] = useState([])
+    const Headers = [
+        {label:"Prénom", val:(data)=>(data?.prenom)},
+        {label:"Nom", val:(data)=>(data?.nom)},
+        {label:"Entreprise", val:(data)=>(data?.stages.find(s=>s?.statut === 1)?.entreprise)},
+        {label:"Debut/Fin", val:(data)=>{
+            const stage = data?.stages.find(s=>s?.statut === 1)
+            return stage?.debut + " - " + stage?.fin
+        }},
+        {label:"Encadrant", val:(data)=>{
+            const idx = data?.stages.find(s=>s?.statut === 1)?.id_encadrant
+            const sp = supervisors.find(sup=>sup?.id===idx)
+            return <div className="w-fit px-2 rounded-xl border border-gray-200">
+               Prof. {sp?.prenom} {sp?.nom}
+            </div>
+        }},
+    ]
+
+    useEffect(()=>{
+    const fetchData = async ()=>{
+        try{
+            const resp = await api.get("api/students/stages")
+            setStudents(resp.data.filter(std=>std?.stages.find(s=>s?.statut === 1)))
+
+            const respSup = await api.get(`api/supervisors`)
+            setSupervisors(respSup.data)
+        } catch(err){
+            console.log(err)
+        }
+    }
+
+    fetchData()
+
+    },[])
+
+  return (
+    <div>
+        <Table Headers={Headers} Data={students}/>
+    </div>
+  )
+}
+
+export default StudentTrackingView
